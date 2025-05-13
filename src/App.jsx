8811d91632c4819fae5de28916147bd7c59aa1b3
Shubh <shubh2268@gmail.com>
2025-05-13 20:49:05 +0530

    --

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
-
+import About from './pages/About'
+import Services from './pages/Services'
+import Blog from './pages/Blog'
+import Contact from './pages/Contact'
 
 function App() {
 
@@ -10,6 +13,10 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/blog' element={<Blog />} />
+        <Route path='/contact' element={<Contact />} />
       </Routes>
     </Router>
   )
