3c80c362f165a70ba5b9cda472b777a05d7743ff
Shubh <shubh2268@gmail.com>
2025-05-14 20:19:02 +0530

    --

--- src/components/TextCard.jsx
+++ src/components/TextCard.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
 
-const TextCard = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const TextCard = ({ heading, description, className }) => {
+    return (
+        <div className={className}>
+            <h2 className='font-medium'>{heading}</h2>
+            <p className='text-gray-500'>{description}</p>
+        </div>
+    )
 }
 
 export default TextCard
