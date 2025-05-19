d48b2a9162bf32f4ccdcdae765bc6952c253425e
Shubh <shubh2268@gmail.com>
2025-05-19 18:05:52 +0530

    --

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,8 +3,40 @@ import RectangleImg2 from '../assets/about/RectangleImg2.png'
 import RectangleImg3 from '../assets/about/RectangleImg3.png'
 import RectangleImg4 from '../assets/about/RectangleImg4.png'
 import TextCard from '../components/TextCard'
+import { CgWebsite } from 'react-icons/cg'
+import { IoIosRocket } from 'react-icons/io'
+import { CiMobile3 } from 'react-icons/ci'
+import { FaVrCardboard } from 'react-icons/fa'
 
 const About = () => {
+
+  const services = [
+    {
+      id: 1,
+      icon: <CgWebsite />,
+      title: 'Web Application',
+      style: ''
+    },
+    {
+      id: 2,
+      icon: <IoIosRocket />,
+      title: 'SEO',
+      style: ''
+    },
+    {
+      id: 3,
+      icon: <CiMobile3 />,
+      title: 'Mobile Application',
+      style: ''
+    },
+    {
+      id: 4,
+      icon: <FaVrCardboard />,
+      title: 'AR/VR',
+      style: ''
+    }
+  ]
+
   return (
     <div>
       {/* Top  */}
@@ -34,7 +66,20 @@ const About = () => {
 
       {/* Heading  */}
       <div className='my-20'>
-        <TextCard heading='Crafting digital journeys that drive impact, innovation, and identity' description="At Digitra, we specialize in turning ideas into immersive digital experiences that captivate, convert, and grow with your brand. From intuitive user interfaces to performance-driven web solutions, we merge creativity with strategy to help businesses make a lasting impression. Our team is driven by innovation, fueled by collaboration, and committed to delivering work that speaks volumes in a digital-first world." className='flex flex-col items-center justify-center' head_class='font-bold text-[#1090BC] text-2xl md:text-3xl lg:text-4xl text-center mx-3 lg:mx-0' desc_class='p-3 text-gray-600 w-full md:w-3/5 text-center' />
+        <TextCard heading='Crafting digital journeys that drive impact, innovation, and identity' description="At Digitra, we blend creativity with technology to build powerful digital experiences. From innovative designs to scalable web solutions, our mission is to help businesses grow, connect, and thrive in the digital age." className='flex flex-col items-center justify-center' head_class='font-bold text-[#1090BC] text-2xl md:text-3xl text-center mx-3 lg:mx-0' desc_class='p-3 text-gray-600 w-full md:w-3/5 text-center' />
+      </div>
+
+      {/* Bottom  */}
+      <div>
+          <div className='w-full md:w-1/2 md:ml-14'>
+            <TextCard heading='Driven by Creativity, Focused on Results' description="We turn ideas into impactful digital solutions. Through a blend of design, strategy, and technology, we help brands grow, connect, and stand out in an ever-evolving digital landscape." className='flex flex-col items-start' head_class='py-5 text-xl md:text-2xl font-bold text-gray-700' desc_class='w-3/5 text-gray-600' />
+
+            <button className='mt-5 px-8 py-2 bg-transparent border-2 border-[#1090CB] text-[#1090CB] rounded-md font-medium transition active:scale-95 duration-300 cursor-pointer'>Contact Us</button>
+          </div>
+
+          <div>
+
+          </div>
       </div>
 
     </div>
