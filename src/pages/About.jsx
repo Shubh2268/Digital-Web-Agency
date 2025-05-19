1c3069a836ed7dfaafb83b0a6642db39ff48e329
Shubh <shubh2268@gmail.com>
2025-05-19 20:20:31 +0530

    --

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -39,7 +39,7 @@ const About = () => {
   ]
 
   return (
-    <div>
+    <div className='bg-gray-50'>
       {/* Top  */}
       <div className='max-w-7xl mx-auto pt-28 md:pt-32'>
         <div className='grid grid-cols-1 lg:grid-cols-2 gap-x-9 gap-y-3 items-start lg:m-10 xl:m-0'>
@@ -78,7 +78,7 @@ const About = () => {
           <button className='mt-5 px-8 py-2 bg-transparent border-2 border-[#1090CB] text-[#1090CB] rounded-md font-medium transition active:scale-95 duration-300 cursor-pointer'>Contact Us</button>
         </div>
 
-        <div className='w-full md:w-1/2'>
+        <div className='w-full md:w-1/2 bg-white p-5'>
           <div className='grid grid-cols-2'>
             {services.map(({ id, title, icon, style }) => (
               <div key={id} className='flex items-center justify-center w-fit m-5'>
