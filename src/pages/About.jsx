73a2f429693fceb96d36e5e077b5fc397d448a44
Shubh <shubh2268@gmail.com>
2025-05-20 09:58:55 +0530

    --

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -67,19 +67,19 @@ const About = () => {
 
       {/* Heading  */}
       <div className='my-20'>
-        <TextCard heading='Crafting digital journeys that drive impact, innovation, and identity' description="At Digitra, we blend creativity with technology to build powerful digital experiences. From innovative designs to scalable web solutions, our mission is to help businesses grow, connect, and thrive in the digital age." className='flex flex-col items-center justify-center' head_class='font-bold text-[#1090BC] text-2xl md:text-3xl text-center mx-3 lg:mx-0' desc_class='p-3 text-gray-600 w-full md:w-3/5 text-center' />
+        <TextCard heading='Crafting digital journeys that drive impact, innovation, and identity' description="At Digitra, we blend creativity with technology to build powerful digital experiences. From innovative designs to scalable web solutions, our mission is to help businesses grow, connect, and thrive in the digital age." className='flex flex-col items-center justify-center' head_class='font-bold text-[#1090BC] text-2xl lg:text-3xl text-center mx-3 lg:mx-0' desc_class='p-3 text-gray-600 w-full md:w-4/5 lg:w-3/5 text-center' />
       </div>
 
       {/* Bottom  */}
-      <div className='flex flex-col md:flex-row items-center justify-center mt-20 mb-32'>
-        <div className='w-full md:w-1/2 md:ml-20'>
-          <TextCard heading='Driven by Creativity, Focused on Results' description="We believe in creating more than just beautiful interfaces — we build meaningful digital experiences. Our multidisciplinary team collaborates with forward-thinking brands to craft solutions that engage, inspire, and deliver real results. By blending creativity with strategy and technology, we help businesses grow with confidence in the digital era." className='flex flex-col items-start' head_class='py-5 text-xl md:text-2xl font-bold text-gray-700' desc_class='w-[90%] text-gray-600' />
+      <div className='flex flex-col lg:flex-row items-center justify-center mt-20 mb-32'>
+        <div className='w-[90%] lg:w-1/2 md:ml-20 md:mr-10'>
+          <TextCard heading='Driven by Creativity, Focused on Results' description="We believe in creating more than just beautiful interfaces — we build meaningful digital experiences. Our multidisciplinary team collaborates with forward-thinking brands to craft solutions that engage, inspire, and deliver real results. By blending creativity with strategy and technology, we help businesses grow with confidence in the digital era." className='flex flex-col items-center md:items-start' head_class='py-5 text-xl md:text-2xl font-bold text-gray-700' desc_class='text-gray-600' />
 
           <button className='mt-7 px-8 py-2 bg-transparent border-2 border-[#1090CB] text-[#1090CB] rounded-md font-medium transition active:scale-95 duration-300 cursor-pointer'>Contact Us</button>
         </div>
 
-        <div className='w-full md:w-1/2 bg-white shadow-lg shadow-gray-300 p-10'>
-          <div className='grid grid-cols-2'>
+        <div className='w-full lg:w-1/2 bg-white shadow-lg shadow-gray-300 p-5 md:p-10 mt-10 lg:mt-0'>
+          <div className='grid grid-cols-1 md:grid-cols-2'>
             {services.map(({ id, title, icon, style }) => (
               <div key={id} className='flex items-center justify-center w-fit m-5'>
                 <div className={`p-4 rounded-2xl text-xl md:text-3xl ${style}`}>{icon}</div>
