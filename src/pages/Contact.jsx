b34ddd31824513615aa06af75662762a8200930c
Shubh <shubh2268@gmail.com>
2025-05-24 12:54:51 +0530

    --

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -15,24 +15,24 @@ const Contact = () => {
             <p className='text-gray-600 lg:text-lg w-full md:w-4/5'>We're here to help bring your ideas to life with seamless digital solutions tailored for you.</p>
 
             <div className='mt-10 flex flex-col-reverse md:flex-row items-center'>
-              <div className='flex items-center gap-4 py-1 md:py-0 md:pr-3'>
+              <div className='flex items-center gap-4 md:gap-2 py-1 md:py-0 md:pr-3'>
                 <a href='#' className='text-blue-700 p-2 rounded-full shadow-lg shadow-gray-300'><FaFacebookF className='w-3 h-3' /></a>
-                <a href='#' className='text-pink-700 p-2 rounded-full shadow-lg shadow-gray-300'><FaInstagram className='w-3 h-3' /></a>
-                <a href='#' className='text-gray-900 p-2 rounded-full shadow-lg shadow-gray-300'><FaXTwitter className=' w-3 h-3' /></a>
+                <a href='#' className='text-pink-600 p-2 rounded-full shadow-lg shadow-gray-300'><FaInstagram className='w-3 h-3' /></a>
+                <a href='#' className='text-black p-2 rounded-full shadow-lg shadow-gray-300'><FaXTwitter className=' w-3 h-3' /></a>
                 <a href='#' className='text-blue-700 p-2 rounded-full shadow-lg shadow-gray-300'><FaLinkedinIn className='w-3 h-3' /></a>
               </div>
 
-              <div className='hidden md:flex border-l border-gray-300 h-6'></div>
+              <div className='hidden md:flex border-l border-gray-500 h-6'></div>
 
               <div className='flex items-center space-x-2 py-2 md:py-0 md:px-3'>
-                <BiSolidPhoneCall />
+                <BiSolidPhoneCall className='text-gray-700 w-5 h-5' />
                 <span className='text-gray-700'> +91 12345 67890</span>
               </div>
 
-              <div className='hidden md:flex border-l border-gray-300 h-6'></div>
+              <div className='hidden md:flex border-l border-gray-500 h-6'></div>
 
               <div className='flex items-center space-x-2 py-2 md:py-0 md:px-3'>
-                <IoLocationSharp />
+                <IoLocationSharp className='text-gray-700 w-5 h-5' />
                 <span className='text-gray-700'> New Delhi, India</span>
               </div>
             </div>
